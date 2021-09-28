3b4ba6d0788d1ae5e0e472b25c2b6a78857ec21d
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-09-28 15:27:03 +0200

    add hero

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
     <Title/>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Hero/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 import Title from './components/Title.vue'
+import Hero from './components/Hero.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    Title
+    // HelloWorld,
+    Title,
+    Hero
   }
 }
 </script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hero">
+    <img src="../assets/logo.png" alt="logo Vue">
+    <h2>Se lo capisci Vue CLI ti semplifica la vita!!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero'
+}
+</script>
+
+<style>
+  .hero{
+    padding: 3rem;
+    background-color: #90bba8;
+  }
+  h2{
+    color: #fff;
+    font-size: 3rem;
+  }
+</style>
